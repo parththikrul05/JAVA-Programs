3bdf35a2d6805eee2fe2f22b781d8fb125812562
parththikrul05 <parth.thirul.btech2025@sitpune.edu.in>
2026-08-02 14:54:33 +0530

    Updated Mobile.java

--- Assignment-2/Mobile.java
+++ Assignment-2/Mobile.java
@@ -1,40 +1,40 @@
-public class Mobile {
+public class Mobile {      //Mobile class
     String model_name;
     String colour;
     int battery_life;
     int mobile_price;
 
-    void print_details(){
+    void print_details(){         //function for displaying details
         System.out.println("Model:"+model_name);
         System.out.println("Colour:"+colour);
         System.out.println("Battery Life:"+battery_life+" hours");
         System.out.println("Price:$"+mobile_price);
     }
-    Mobile(){
+    Mobile(){    //Default Constructor
         model_name = "Google Pixel R9";
         colour = "Vanta Black";
         battery_life = 14;
         mobile_price = 1200;
     }
-    Mobile(String mod,String col,int bat,int p){
+    Mobile(String mod,String col,int bat,int p){   //Parameterized Constructor
             model_name = mod;
             colour = col;
             battery_life = bat;
             mobile_price = p;
     }
 
-    Mobile(Mobile m){
+    Mobile(Mobile m){  //Copy Constructor
         model_name = m.model_name;
         colour = m.colour;
         battery_life = m.battery_life;
         mobile_price = m.mobile_price;
     }
 
-    public static void main(String[] args) {
-        Mobile m1 = new Mobile();
+    public static void main(String[] args) {      //Main function
+        Mobile m1 = new Mobile();  //Default constructor object
         m1.print_details();
         System.out.print("\n");
-        Mobile m2 = new Mobile("Iphone","Red",9,2400); 
+        Mobile m2 = new Mobile("Iphone","Red",9,2400);  //Parameterized constructor Object
         m2.print_details();
         System.out.print("\n");
         Mobile m3 = new Mobile(m2);   //Copy Constructor
